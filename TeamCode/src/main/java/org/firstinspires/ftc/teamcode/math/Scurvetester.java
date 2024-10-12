6faa0d708f7366c23d48674c269e9a06b1d9b8b0
tarunja1ks <tarunjaiks@gmail.com>
2024-10-12 11:25:25 -0700

    fixing some of the calculating parameters but still need to fix as

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/Scurvetester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/Scurvetester.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode.math;
 
 public class Scurvetester {
     public static void main(String[]args){
-        Scurve curves=new Scurve(0,1,);
+        Scurve curves=new Scurve(0,1);
+//        System.out.println(curves.getParameters());
+        double finalPos=curves.T*2+curves.line_length;
+        System.out.println(curves.getPosition(finalPos/2));
+//        System.out.println(curves.getPosition(finalPos/4));
     }
 }
