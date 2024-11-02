dc60226769019bf9632dd7ff5fbd8734e02bb5aa
aaguy-hue <aaditmathur6@gmail.com>
2024-11-02 02:48:03 -0700

    changes to dt interface

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/drive/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/drive/Drivetrain.java
@@ -3,8 +3,27 @@ package org.firstinspires.ftc.teamcode.robot.drive;
 import androidx.annotation.NonNull;
 
 import org.firstinspires.ftc.teamcode.math.Coordinate;
+import org.firstinspires.ftc.teamcode.math.Pose2d;
 
 public interface Drivetrain {
+    /**
+     * Drive at max speed.
+     * @param direction The direction the robot should drive in (NOT FIELD CENTRIC)
+     */
+    default void move(@NonNull Pose2d direction) {
+        this.move(Coordinate.fromPose(direction), direction.heading);
+    }
+
+    /**
+     * Drive at a certain speed.
+     * @param direction The direction the robot should drive in (NOT FIELD CENTRIC)
+     * @param speed     The robot's speed, a value between 0 and 1 (this is a multiplier, will which
+     *                  basically sets the max speed).
+     */
+    default void move(@NonNull Pose2d direction, double speed) {
+        this.move(Coordinate.fromPose(direction), direction.heading, speed);
+    }
+
     /**
      * Drive at a certain speed.
      * @param velocity The x and y.
@@ -16,7 +35,7 @@ public interface Drivetrain {
         velocity.mult(speed);
         rotation *= speed;
         this.move(velocity, rotation);
-    };
+    }
 
     /**
      * Drive at max speed (1).
