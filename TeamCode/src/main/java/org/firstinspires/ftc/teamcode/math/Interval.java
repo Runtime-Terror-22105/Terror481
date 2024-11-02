88775cb1e141bdc3d835c51f007765db2c8d254a
aaguy-hue <aaditmathur6@gmail.com>
2024-11-02 02:02:29 -0700

    fix mistake and add javadoc to interval class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/Interval.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/Interval.java
@@ -1,37 +1,58 @@
 package org.firstinspires.ftc.teamcode.math;
 
-import com.acmerobotics.roadrunner.util.NanoClock;
-
-// A simple class to repeat a certain action after a certain time period
+/**
+ * A simple class to repeat a certain action after a specified time period.
+ */
 public class Interval {
     private double pollTime;
     private double lastPollTime;
-    private final NanoClock timer;
 
-    // pollTime is in milliseconds
+    /**
+     * Constructs an Interval with the specified poll time.
+     *
+     * @param pollTime the time period in milliseconds after which the action should be repeated
+     */
     public Interval(double pollTime) {
         this.pollTime = pollTime;
-        this.timer = NanoClock.system();
-        this.lastPollTime = 0;
+        this.lastPollTime = System.currentTimeMillis();
     }
 
+    /**
+     * Checks if the specified interval time has passed since the last poll.
+     *
+     * @return true if the interval has passed, false otherwise
+     */
     public boolean intervalHasPassed() {
-        if (getTimeMillis() >= (lastPollTime + pollTime)) {
-            this.lastPollTime = this.getTimeMillis();
+        if (getCurrentTimeMillis() >= (lastPollTime + pollTime)) {
+            this.lastPollTime = this.getCurrentTimeMillis();
             return true;
         }
-
         return false;
     }
 
-    public double getTimeMillis() {
-        return this.timer.seconds()*1e3;
+    /**
+     * Gets the current time in milliseconds.
+     *
+     * @return the current time in milliseconds
+     */
+    public double getCurrentTimeMillis() {
+        return System.nanoTime() / 1e6;
     }
 
+    /**
+     * Gets the last poll time in milliseconds.
+     *
+     * @return the last time the interval was checked in milliseconds
+     */
     public double getLastPollTime() {
         return this.lastPollTime;
     }
 
+    /**
+     * Sets a new poll time.
+     *
+     * @param pollTime the new time period in milliseconds for the interval
+     */
     public void setPollTime(double pollTime) {
         this.pollTime = pollTime;
     }
