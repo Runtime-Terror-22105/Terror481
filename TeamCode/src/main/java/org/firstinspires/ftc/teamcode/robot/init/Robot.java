6225e76ec64cef1d8ecc14f40ae945c0ce56f490
aaguy-hue <aaditmathur6@gmail.com>
2024-11-15 23:51:30 -0800

    remove a lot of legacy code from old desgins, gbeigin robot states

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/init/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/init/Robot.java
@@ -8,14 +8,11 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.math.Coordinate;
 import org.firstinspires.ftc.teamcode.math.Pose2d;
 import org.firstinspires.ftc.teamcode.robot.drive.Drivetrain;
 import org.firstinspires.ftc.teamcode.robot.drive.localizer.OTOSLocalizer;
 import org.firstinspires.ftc.teamcode.robot.drive.mecanum.MecanumDrivetrain;
-import org.firstinspires.ftc.teamcode.robot.hardware.sensors.SampleColor;
 import org.firstinspires.ftc.teamcode.robot.hardware.sensors.camera.TerrorCamera;
-import org.firstinspires.ftc.teamcode.robot.subsystems.InOutTake;
 import org.firstinspires.ftc.teamcode.robot.subsystems.PinkArm;
 
 /**
@@ -23,17 +20,19 @@ import org.firstinspires.ftc.teamcode.robot.subsystems.PinkArm;
  */
 @Config
 public class Robot {
+    // otos dash vals
     public static double OTOS_LINEAR_SCALAR = 1.1371628089414737246168002475814;
     public static double OTOS_ANGULAR_SCALAR = 1.0066;
-
-    // dash
     public static Pose2d OTOS_OFFSET = new Pose2d(0, 3.55, 0);
     public static Pose2d ROBOT_INITIAL_POS = new Pose2d(0, 0, 0);
+
+    // States
+    public RobotState robotState = RobotState.NONE;
 
     // Subsystems
     public Drivetrain drivetrain = null;
     public PinkArm pinkArm = null;
-    public InOutTake inOutTake = null;
+//    public InOutTake inOutTake = null;
 
     // Localizer
     public OTOSLocalizer localizer;
@@ -59,7 +58,7 @@ public class Robot {
 
         // Set up subsytems
         this.pinkArm = new PinkArm(hardware);
-        this.inOutTake = new InOutTake(hardware);
+//        this.inOutTake = new InOutTake(hardware);
 
         // Initialize the localizer
         this.localizer = new OTOSLocalizer(hardware.otos);
@@ -79,61 +78,51 @@ public class Robot {
         );
     }
 
-    /**
-     * Intake a sample
-     * @param colors The colors to intake
-     * @param armPosition The position of the arm
-     */
-    public void intake(SampleColor[] colors, PinkArm.State armPosition) {
-        this.inOutTake.intake(colors);
-        this.pinkArm.setState(armPosition);
-    }
-
-    /**
-     * Outtake a sample
-     * @param armPosition The position of the arm
-     */
-    public void outtake(PinkArm.State armPosition) {
-        this.inOutTake.outtake();
-        this.pinkArm.setState(armPosition);
-    }
-
-    /**
-     * Move the arm manually
-     * @param pitch The amount to add to the pitch, in radians
-     * @param extension The amount to add to the extension, in inches
-     */
-    public void moveArm(double pitch, double extension) {
-        this.pinkArm.adjustPitch(pitch);
-        this.pinkArm.adjustExtension(extension);
-    }
-    
-    public void rotateArmRaw(double power) {
-        this.pinkArm.setPitchPower(power);
-    }
-
-    /**
-     * Hang the robot, toggling between various stages of hang
-     * CALL THIS FUNCTION EVERY LOOP ITERATION
-     */
-    public void hang() {
-        if (!this.pinkArm.isHanging()) {
-            this.pinkArm.setState(PinkArm.State.HANG_1);
-        } else if (this.pinkArm.atTargetPosition() &&
-                this.pinkArm.stateIs(PinkArm.State.HANG_1)) {
-            this.hardware.dtPto.setPosition(1 /* idk smth this is placeholder */);
-
-            // set a 1 second timer before going to hang 2
-            if (this.hangTimer == 0) {
-                this.hangTimer = System.nanoTime();
-            } else if (((System.nanoTime() - this.hangTimer) / 1e9) >= 1) {
-                this.pinkArm.setState(PinkArm.State.HANG_2);
-                this.drivetrain.move(new Coordinate(1, 1), 0);
-            }
-        }
-    }
-
     public void setRobotInitialPos(Pose2d initialPos){
         this.ROBOT_INITIAL_POS=initialPos;
+    }
+
+    /**
+     * Change the robot's state if it is not in the correct state.
+     * @param desiredState The desired state of the robot.
+     * @return Whether or not the state was changed. If it is already in that state, returns false.
+     */
+    public boolean setState(RobotState desiredState) {
+        if (robotState.equals(desiredState)) {
+            return false;
+        }
+        robotState = desiredState;
+
+        switch (robotState) {
+            case RESTING:
+                goToRestingState();
+            case INTAKE:
+                goToIntakeState();
+            case BUCKET:
+                goToBucketState();
+            case SPECIMEN:
+                goToSpecimenState();
+        }
+
+        pinkArm.update();
+
+
+        return true;
+    }
+
+    private void goToRestingState() {
+
+    }
+
+    private void goToIntakeState() {
+
+    }
+
+    private void goToBucketState() {
+
+    }
+
+    private void goToSpecimenState() {
+
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/init/RobotState.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.robot.init;
+
+public enum RobotState {
+    NONE, // only use this at the very start
+    RESTING,
+    INTAKE,
+    BUCKET,
+    SPECIMEN
+}
